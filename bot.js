029e12e5f28e98f63efb6b94edee2c6a8df66168
unknown <dupycournau@gmail.com>
2020-11-11 19:49:22 +0100

    Ajout de fonctionnalité

--- bot.js
+++ bot.js
@@ -19,22 +19,36 @@ client.on('ready', () => {
 
 client.on('message', msg => {
     if(!msg.content.startsWith(prefix) || msg.author.bot) return;
-
-    // const args = msg.content.slice(prefix.length).trim().split('/ +/');
-    // const command = args.shift().toLowerCase();
+    const command = msg.content.split(" ")[0].toLowerCase()
     let tabMsg = msg.content.split(" ");
     switch(tabMsg[0]) {
+        //Gestion sessions        
         case `${prefix}sessionadd`:
-            let args = msg.content.split("|")[1];
-            let session = args.split(";");
-            console.log(session);
+            var regex = /(?<key>[\S]+):(?<value>[^ "]+|["][\w?! ]*["])/gm;
+            var matches = msg.content.matchAll(regex)
+            var tabArgs = {}
+            for (const match of matches) {
+                console.log(match);
+                var monString = match[2].replace(/\"/g, "");
+                tabArgs[match[1]] = monString;
+            }
+            // Gestion de date
+            var now = new Date(Date.now());
+            var dates = tabArgs["jour"].split("/");
+            var sessionDate = new Date(`2020-${dates[1]}-${dates[0]}`)
+            if (now > sessionDate) {
+                sessionDate.setFullYear(sessionDate.getFullYear() + 1);
+            }
+            var time = tabArgs["heure"].split("H");
+            sessionDate.setHours(time[0])
+            sessionDate.setMinutes(time[1]);
+            // Fin de gestion de date
+
             let newSession = {
                 id: tabSession.length,
-                game: session[0],
-                title: session[1],
-                date: session[2],
-                hour: session[3],
-                desc: session[4],
+                game: tabArgs["jeu"],
+                time: sessionDate,
+                desc: tabArgs["desc"],
             };
             tabSession.push(newSession);
             msg.reply("Ta session a bien été ajouté !");
@@ -53,16 +67,8 @@ client.on('message', msg => {
             else
             {
                 console.log("Pas trouvé")
+                msg.reply("La session que tu as voulu supprimer n'as pas été trouvé, essaye encore");
             }
-            break;
-
-        case `${prefix}listsessions`:
-            let listeSessions = "";
-            tabSession.forEach(session => {
-                console.log("Ok Lets go !");
-                listeSessions += `**Titre** : ${session.title} , **Jeu** : ${session.game} **Jour** : ${session.date} **Heure** : ${session.hour} \n`;
-            });
-            myChannelTabAffiche.send(listeSessions);
             break;
         
         case `${prefix}registerTabAffiche`:
@@ -117,6 +123,7 @@ client.on('message', msg => {
             msg.channel.send("Message a modifier")
             .then(message => messageModify = message)
             .catch(console.error);
+            editSessionBoard();
             break;
         case `${prefix}editlol`:
             messageModify.edit("Bonjour la famille !")
@@ -132,16 +139,38 @@ function editSessionBoard()
 {
     if (messageModify != "") {
         var listeSessions = "";
+        if(!tabSession.length > 0) {
+            messageModify.edit("Aucune session prévu ...")
+            .then(console.log("Board updated"))
+            .catch(console.error);
+        }else {
         tabSession.forEach(session => {
-            console.log("Ok Lets go !");
-            listeSessions += `**Numero** : ${session.id} **Titre** : ${session.title} , **Jeu** : ${session.game} **Jour** : ${session.date} **Heure** : ${session.hour} \n`;
+            var jour = session.time.getDate()
+            var jourName = getDayName(session.time.getDay());
+            var mois = getMonthName(session.time.getMonth());
+            var heureMinute = session.time.getHours() + "H" + session.time.getMinutes();
+            listeSessions += `${session.id} | **${session.game}** , **${jourName} ${jour} ${mois}** à _${heureMinute}_ : ${session.desc}\n`;
+            console.log(listeSessions);
         })
         messageModify.edit(listeSessions)
         .then(console.log("Board updated"))
         .catch(console.error);
+        }
     } else {
         console.log("Aucun message editable ajouté, utiliser commande $printEdit");
     }
+}
+
+function getDayName(dayNum)
+{   
+    var dayName = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"];
+    return dayName[dayNum];
+}
+
+function getMonthName(monthNum)
+{
+    var monthName = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Novembre", "Décembre"];
+    return monthName[monthNum];
 }
 
 client.login(process.env.API_KEY);
